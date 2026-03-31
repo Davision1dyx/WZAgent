8bde3bb4a6d24727bed6dff8edd9780e35a816ad
Dav1son <davepz11@126.com>
2026-03-31 21:45:29 +0800

    feat.新增检索方式，使用advisor实现。通过advisorSpec传入metadata过滤参数

--- rag/src/main/java/org/davision1dyx/rag/controller/RetrievalController.java
+++ rag/src/main/java/org/davision1dyx/rag/controller/RetrievalController.java
@@ -1,6 +1,8 @@
 package org.davision1dyx.rag.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
@@ -8,6 +10,7 @@ import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -73,5 +76,44 @@ public class RetrievalController {
 
         return chatModel.call(promptTemplate.create(Map.of("documents", documentContent, "question", query)))
                 .getResult().getOutput().getText();
+    }
+
+    /**
+     * 通过advisor自动完成从向量数据库中检索的操作
+     * @param query
+     * @return
+     */
+    @GetMapping("retrievalAdvisor")
+    public String retrievalAdvisor(@RequestParam String query) {
+        PromptTemplate promptTemplate = new PromptTemplate("""
+                请基于以下提供的参考文档内容，回答用户的问题。
+                如果参考文档中没有相关信息，请直接说明"没有找到相关信息"，不要编造内容。
+                
+                参考文档:
+                {question_answer_context}
+                
+                用户问题: {query}
+                """);
+
+        SearchRequest searchRequest = SearchRequest.builder()
+                .topK(4)
+                .query(query)
+                .similarityThreshold(0.7)
+//                .filterExpression("fileName == \"kubernates in action\"")
+                .build();
+
+        ChatClient chatClient = ChatClient.builder(chatModel)
+                .defaultAdvisors(
+                    QuestionAnswerAdvisor.builder(vectorStore)
+                        .searchRequest(searchRequest)
+                        .promptTemplate(promptTemplate)
+                        .build()
+                ).build();
+
+        return chatClient.prompt()
+                .advisors(advisorSpec -> advisorSpec.param("qa_filter_expression", "fileName == \"kubernates in action\""))
+                .user(query)
+                .call()
+                .content();
     }
 }
